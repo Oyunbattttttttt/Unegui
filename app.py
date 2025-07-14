6bd7139544bf28bf31d01996268f2db4af35f2b9
Oyunbattttttttt <oyunbat1018@gmail.com>
2025-07-14 12:04:05 +0800

    dd

--- app.py
+++ app.py
@@ -84,6 +84,7 @@ def index():
 
     return render_template("index.html", chart=chart_html)
 
-from flask import Flask
-app = Flask(__name__)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
